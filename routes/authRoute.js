56ec1b0b358cb142039b4a2f734c31e9a2039d02
radhikaguptaa786 <radhikaguptaa786@gmail.com>
2024-03-27 01:21:55 +0530

    admin Panel

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { registerController,loginController ,testController} from "../controllers/authControllers.js";
 
-import { requireSignIn } from "../middlewares/authMiddleware.js";
+import { requireSignIn,isAdmin } from "../middlewares/authMiddleware.js";
 // router object
 const router = express.Router();
 
@@ -13,6 +13,6 @@ router.post("/register", registerController);
 router.post('/login',loginController)
 
 // test routes
-router.get('/test',requireSignIn,testController)
+router.get('/test',requireSignIn,isAdmin,testController)
 
 export default router;
